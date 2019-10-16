fa8a3ee1745dc9bbf7625e7baec2e590b5bbd3ae
shandinan <shandinan@live.com>
2019-10-16 16:07:17 +0800

    sdn

--- winForm_test/Form1.Designer.cs
+++ winForm_test/Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.btnInit = new System.Windows.Forms.Button();
+            this.btnStartRec = new System.Windows.Forms.Button();
+            this.btnStopRec = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnInit
             // 
-            this.btnInit.Location = new System.Drawing.Point(57, 67);
+            this.btnInit.Location = new System.Drawing.Point(12, 57);
             this.btnInit.Name = "btnInit";
             this.btnInit.Size = new System.Drawing.Size(75, 23);
             this.btnInit.TabIndex = 0;
@@ -41,11 +43,33 @@
             this.btnInit.UseVisualStyleBackColor = true;
             this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
             // 
+            // btnStartRec
+            // 
+            this.btnStartRec.Location = new System.Drawing.Point(118, 67);
+            this.btnStartRec.Name = "btnStartRec";
+            this.btnStartRec.Size = new System.Drawing.Size(75, 23);
+            this.btnStartRec.TabIndex = 1;
+            this.btnStartRec.Text = "开始录像";
+            this.btnStartRec.UseVisualStyleBackColor = true;
+            this.btnStartRec.Click += new System.EventHandler(this.btnStartRec_Click);
+            // 
+            // btnStopRec
+            // 
+            this.btnStopRec.Location = new System.Drawing.Point(118, 118);
+            this.btnStopRec.Name = "btnStopRec";
+            this.btnStopRec.Size = new System.Drawing.Size(75, 23);
+            this.btnStopRec.TabIndex = 2;
+            this.btnStopRec.Text = "结束录像";
+            this.btnStopRec.UseVisualStyleBackColor = true;
+            this.btnStopRec.Click += new System.EventHandler(this.btnStopRec_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnStopRec);
+            this.Controls.Add(this.btnStartRec);
             this.Controls.Add(this.btnInit);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +80,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.Button btnStartRec;
+        private System.Windows.Forms.Button btnStopRec;
     }
 }
 
